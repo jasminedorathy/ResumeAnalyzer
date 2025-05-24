0161d9c8cc34e6b210cb3b5176afff149f4a2a40
Aashif786 <jasminedorathyvasantharaj@gmail.com>
2025-05-24 13:58:55 +0530

    Initial commit

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\mp3\resume-analyser\App\myenv
+set VIRTUAL_ENV=D:\ResumeAnalyzer\resume-analyser\App\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
